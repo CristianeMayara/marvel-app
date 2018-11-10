aa10080ce472d4702bcb58a8f19e519439085850
CristianeMayara <cris.mayara@gmail.com>
2018-11-10 18:57:36 -0300

    Setting card components

--- src/views/CharacterList.js
+++ src/views/CharacterList.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { Col, Row, Card, CardTitle, Input } from "react-materialize";
 import { fetchCharacters, searchCharacters } from "../actions/CharacterAction";
 
 class CharacterList extends Component {
@@ -22,26 +23,49 @@ class CharacterList extends Component {
     else this.props.fetchCharacters();
   }
 
-  render() {
-    console.log("characterList", this.props.characterList);
+  renderItem(item, index) {
     return (
-      <div>
-        <h1>Character List</h1>
-        <input
-          name="name"
-          type="text"
-          placeholder="Enter character name"
-          onChange={this.handleChangeInput}
-        />
-        {this.props.characterList.characters &&
-          this.props.characterList.characters.map(character => (
-            <div key={character.id}>
-              {character.name}
-              <Link to={`/character/${character.id}`}>Edit</Link>
-              <Link to={`/details/${character.id}`}>See details</Link>
-            </div>
-          ))}
-      </div>
+      <Col m={4} s={12} key={index}>
+        <Card
+          className="large"
+          header={
+            <CardTitle
+              image={`${item.thumbnail.path}.${item.thumbnail.extension}`}
+            >
+              {item.name}
+            </CardTitle>
+          }
+          actions={[
+            <Link to={`/character/${item.id}`}>Edit</Link>,
+            <Link to={`/details/${item.id}`}>See details</Link>
+          ]}
+        >
+          I am a very simple card. I am good at containing small bits of
+          information. I am convenient because I require little markup to use
+          effectively.
+        </Card>
+      </Col>
+    );
+  }
+
+  render() {
+    return (
+      <Row>
+        <div>
+          <h1>Character List</h1>
+          <Input
+            s={12}
+            name="name"
+            type="text"
+            placeholder="Search for a character"
+            onChange={this.handleChangeInput}
+          />
+          {this.props.characterList.characters &&
+            this.props.characterList.characters.map((item, index) =>
+              this.renderItem(item, index)
+            )}
+        </div>
+      </Row>
     );
   }
 }
