60d20cae96f65b9f75ae1fac6af952d9b62380d8
CristianeMayara <cris.mayara@gmail.com>
2018-11-11 18:26:19 -0300

    Adding personal data in details screen

--- src/views/CharacterDetails.js
+++ src/views/CharacterDetails.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Collection, CollectionItem } from "react-materialize";
-import { fetchSeries } from "../actions/CharacterAction";
+import { Tab, Tabs, Card, Collection, CollectionItem } from "react-materialize";
+import { fetchSeries, fetchCharacter } from "../actions/CharacterAction";
 import BaseView from "../components/BaseView";
 
 class CharacterDetails extends Component {
@@ -10,27 +10,56 @@ class CharacterDetails extends Component {
   }
 
   componentDidMount() {
+    this.props.fetchCharacter(this.props.match.params.id);
     this.props.fetchSeries(this.props.match.params.id);
   }
 
   renderItem(item, index) {
     return (
-      <CollectionItem key={index} href={item.resourceURI}>
-        {item.name}
-      </CollectionItem>
+      <Card key={index}>
+        <CollectionItem key={index} href={item.resourceURI}>
+          {item.name}
+        </CollectionItem>
+      </Card>
     );
   }
 
+  renderSeries() {}
+  renderDetails() {}
+
   render() {
+    console.log(this.props.character);
     return (
       <BaseView>
-        <Collection>
-          {this.props.serieList &&
-            this.props.serieList.series.available &&
-            this.props.serieList.series.items.map((item, index) =>
-              this.renderItem(item, index)
-            )}
-        </Collection>
+        <Tabs className="tab-demo z-depth-1">
+          <Tab title="Personal data" active>
+            <Card>
+              <p>
+                <strong>Abilities:</strong> abc
+              </p>
+              <p>
+                <strong>Species:</strong> abc
+              </p>
+              <p>
+                <strong>First appearance:</strong> abc
+              </p>
+              <p>
+                <strong>Description:</strong> abc
+              </p>
+            </Card>
+          </Tab>
+          <Tab title="Series">
+            <Card>
+              <Collection>
+                {this.props.serieList &&
+                  this.props.serieList.series.available &&
+                  this.props.serieList.series.items.map((item, index) =>
+                    this.renderItem(item, index)
+                  )}
+              </Collection>
+            </Card>
+          </Tab>
+        </Tabs>
       </BaseView>
     );
   }
@@ -38,12 +67,14 @@ class CharacterDetails extends Component {
 
 const mapStateToProps = state => {
   return {
+    character: state.characterStore.editCharacter.character,
     serieList: state.characterStore.serieList
   };
 };
 
 const mapDispathToProps = dispatch => {
   return {
+    fetchCharacter: id => dispatch(fetchCharacter(id)),
     fetchSeries: characterId => dispatch(fetchSeries(characterId))
   };
 };
